c5513fe2d211732a35ed296d5095acadc8c57c2a
Marius Appel <marius.appel@uni-muenster.de>
2015-01-14 15:28:43 +0100

    Added v2 performance test

--- examples/benchmark/reduce_scalability_v2.R
+++ examples/benchmark/reduce_scalability_v2.R
@@ -1,10 +1,11 @@
 
 ITERATIONS = 3 #5
-NQUERIES = 10 #10
-NT = 10   # 30
-NM = 50 # 500
-NSERVERMAX = 10
+NQUERYMAX = 10 #10 <= NSERVER
+NT = 100   # 30
+NM = 100 # 500
+NSERVER= 10
 VERBOSE=T
+
 
 #rasql --user rasadmin --passwd rasadmin -q "select (marray x in [1:5,1:5] values 1f)[1,1] from TestColl" --out string 
 
@@ -19,7 +20,6 @@ system("rasql --user rasadmin --passwd rasadmin -q  'update TestColl as c set c[
 system("rasql --user rasadmin --passwd rasadmin -q  'update TestColl as c set c[0:999, 0:999, 2] assign marray v in [0:999, 0:999] values 2f'",ignore.stdout = !VERBOSE, ignore.stderr = !VERBOSE)
 system("rasql --user rasadmin --passwd rasadmin -q  'update TestColl as c set c[0:999, 0:999, 3] assign marray v in [0:999, 0:999] values 3f'",ignore.stdout = !VERBOSE, ignore.stderr = !VERBOSE)
 
-#system("chmod +x run.sh",ignore.stdout = !VERBOSE, ignore.stderr = !VERBOSE)
 
 # Kill running default servers # down srv -all -kill does not work sometimes
 system("rascontrol -q -x down srv N1 -kill",ignore.stdout = !VERBOSE, ignore.stderr = !VERBOSE)
@@ -34,13 +34,13 @@ system("rascontrol -q -x down srv N9 -kill",ignore.stdout = !VERBOSE, ignore.std
 Sys.sleep(3)
 
 
-for (i in 1:NSERVERMAX) {
+for (i in 1:NSERVER) {
 	# using tile cache leads to segfault errors -> set to 0 (default)
 	system(paste("rascontrol -q -x define srv TEST", i ," -host rasdaman-dev1 -type n -port ", 8001 + i , " -dbh rasdaman_host", sep=""),ignore.stdout = !VERBOSE, ignore.stderr = !VERBOSE) # 64 MB default cache size
-	system(paste("rascontrol -q -x change srv TEST", i ," -countdown 200 -autorestart on -xp --timeout 300", sep=""),ignore.stdout = !VERBOSE, ignore.stderr = !VERBOSE) # 64 MB default cache size
-	system(paste("rascontrol -q -x down srv TEST", i , sep=""),ignore.stdout = !VERBOSE, ignore.stderr = !VERBOSE) # 64 MB default cache size
+	system(paste("rascontrol -q -x change srv TEST", i ," -countdown 10000 -autorestart on -xp --timeout 10000", sep=""),ignore.stdout = !VERBOSE, ignore.stderr = !VERBOSE) # 64 MB default cache size
+	system(paste("rascontrol -q -x up srv TEST", i , sep=""),ignore.stdout = !VERBOSE, ignore.stderr = !VERBOSE) # 64 MB default cache size
 }
-
+Sys.sleep(2)
 cat("DONE.\n")
 
 
@@ -48,24 +48,29 @@ cat("DONE.\n")
 
 cat("STARTING SCALABILITY TEST \n")
 
-result <- data.frame(NT=rep(NT,NSERVERMAX), NM=rep(NM,NSERVERMAX), NSERVER=1:NSERVERMAX, RUNTIME=rep(NA,NSERVERMAX))
-for (i in 1:NSERVERMAX) {
-
-	system(paste("rascontrol -q -x up srv TEST",i,sep=""),ignore.stdout = TRUE, ignore.stderr = TRUE)
-	Sys.sleep(2)
+result <- data.frame(NT=rep(NA,NQUERYMAX), NM=rep(NM,NQUERYMAX), NSERVER=rep(NSERVER,NQUERYMAX), NQUERIES=1:NQUERYMAX, RUNTIME=rep(NA,NQUERYMAX))
+for (i in 1:NQUERYMAX) {
+	nt < ceil(NT / i)
 	ct <- 0
-	cat("USING IMAGE SIZE", NM, "x", NM, "- POINTS IN TIME", NT, "- NQUERIES", NQUERIES, "- RUNNING SERVERS", i)
+	cat("USING IMAGE SIZE", NM, "x", NM, "- POINTS IN TIME", nt, "- NQUERIES", i, "- RUNNING SERVERS", NSERVER)
 	for (z in 1:ITERATIONS) {
 		#ct <- ct + system.time(system(paste("parallel rasql --user rasadmin --passwd rasadmin -q 'select (marray variance in [1:", NM, ",1:", NM, "] values condense + over y in [1:", NT ,"] using (TestColl[variance[0], variance[1], y[0]] - avg_cells(TestColl[variance[0], variance[1], 1:", NT, "])) * (TestColl[variance[0], variance[1], y[0]] - avg_cells(TestColl[variance[0], variance[1], 1:", NT, "])) / ", NT-1 ,"f)[1,1] from TestColl' --out string ::: {1..", NQUERIES, "}", sep=""),ignore.stdout = !VERBOSE, ignore.stderr = !VERBOSE))[3]
-		ct <- ct + system.time(system(paste("./run.sh", NM, NT, NQUERIES),ignore.stdout = !VERBOSE, ignore.stderr = !VERBOSE))[3]
+		ct <- ct + system.time(system(paste("./run_v2.sh", NM, nt, i),ignore.stdout = !VERBOSE, ignore.stderr = !VERBOSE))[3]
 		Sys.sleep(0.5)
 		cat(".")
 	}
 	cat(" TOOK ", round(ct / ITERATIONS, digits=2), "s\n")
 	result[i,"RUNTIME"] = ct / ITERATIONS
+	result[i,"NT"] = nt
 }
 
 save(result, file="result.rda")
+
+
+
+
+
+
 
 
 
@@ -74,7 +79,7 @@ cat("Cleaning up...")
 # Clean up
 
 
-for (i in 1:NSERVERMAX) {
+for (i in 1:NSERVER) {
     system(paste("rascontrol -q -x down srv TEST", i , sep=""),ignore.stdout = !VERBOSE, ignore.stderr = !VERBOSE)
 	Sys.sleep(0.5)
 	system(paste("rascontrol -q -x remove srv TEST", i , sep=""),ignore.stdout = !VERBOSE, ignore.stderr = !VERBOSE)
@@ -96,5 +101,10 @@ system("rascontrol -q -x up srv N9",ignore.stdout = !VERBOSE, ignore.stderr = !V
 
 
 cat("\nDONE.\n\n\n")
+
+
+
+
+
 
 
